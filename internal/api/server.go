33ea1b29f581b7a8b1bd4ff3a40beb5d3bbf87aa
Behnam <behnam789@hotmail.com>
2024-08-25 22:46:40 +0100

    Added swagger for API

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"goQuiz/internal/models"
 	"goQuiz/internal/storage"
@@ -37,9 +40,45 @@ func (s *Server) SubmitAnswers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
+func (s *Server) ServeAPIDoc(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received request for: %s", r.URL.Path)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Printf("Error getting current directory: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	if r.URL.Path == "/api-docs" {
+		filePath := filepath.Join(currentDir, "api-docs.yaml")
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			log.Printf("api-docs.yaml file not found at %s", filePath)
+			http.Error(w, "API documentation not found", http.StatusNotFound)
+			return
+		}
+		log.Printf("Serving api-docs.yaml from: %s", filePath)
+		w.Header().Set("Content-Type", "application/yaml")
+		http.ServeFile(w, r, filePath)
+	} else {
+		filePath := filepath.Join(currentDir, "swagger-ui.html")
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			log.Printf("swagger-ui.html file not found at %s", filePath)
+			http.Error(w, "Swagger UI not found", http.StatusNotFound)
+			return
+		}
+		log.Printf("Serving swagger-ui.html from: %s", filePath)
+		http.ServeFile(w, r, filePath)
+	}
+}
+
 func (s *Server) SetupRoutes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/questions", s.GetQuestions)
 	mux.HandleFunc("/submit", s.SubmitAnswers)
+	mux.HandleFunc("/api-docs", s.ServeAPIDoc)
+	mux.HandleFunc("/docs", s.ServeAPIDoc)
+
+	log.Println("Routes set up. API documentation available at /docs")
+
 	return mux
 }
